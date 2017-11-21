274145f90312d372170c22935fa0bf5758ce0062
Nnenna Ndukwe <nnenna.s.ndukwe@gmail.com>
2017-11-21 12:40:58 -0500

    viewing of all results

--- bettingGame/js/bettingGame.js
+++ bettingGame/js/bettingGame.js
@@ -6,11 +6,11 @@ let arr = ["apples","oranges","cherries","kiwi","pineapples"];
 
 $("#bet1").on("click",function(){
 
-	let betAmount = $("#betAmount").val();
+	let betAmount = parseFloat($("#betAmount").val());
 	
 	if((betAmount !== "") && (betAmount > 0)){
 	
-	console.log(betAmount)
+	$("#betShow").text("$"+betAmount)
 	storedMoney = parseFloat(betAmount);
 
 	$("#bank").text("YOUR BANK: $"+storedMoney);
@@ -22,20 +22,27 @@ $("#bet1").on("click",function(){
 	$("#slotResults").html(slot1+"<br>"+slot2+"<br>"+slot3);
 
 	if((slot1 == slot2) && (slot2 == slot3)){
+		$("#betShow").text("$"+betAmount)
+		$("#slotResults").html(slot1+"<br>"+slot2+"<br>"+slot3);
 		$("#message").text("YOU WIN! YOUR MONEY GETS MULTIPLIED BY 10!");
-		betAmount*=10;
+		betAmount=betAmount*10;
 		storedMoney+=betAmount;
 	}
 	else if((slot1 == slot2) || (slot2 == slot3) || (slot1 == slot3)){
+		$("#betShow").text("$"+betAmount)
+		$("#slotResults").html(slot1+"<br>"+slot2+"<br>"+slot3);
 		$("#message").text("YOU...KINDA WON? HERE'S AN ADDITIONAL 2x YOUR BET.")
 		betAmount = betAmount*2;
 		storedMoney+=betAmount;
 		$("#bank").text("Stored Money: $"+storedMoney)
 	}
 	else{
+		$("#slotResults").html(slot1+"<br>"+slot2+"<br>"+slot3);
 		$("#message").text("YOU LOSE! SORRY, BUDDY.");
+		$("#betShow").text("$"+betAmount)
 		storedMoney-=betAmount;
 		$("#bank").text("Stored Money: $"+storedMoney)
+		$("#betAmount").val("")
 	};
 	}
 
