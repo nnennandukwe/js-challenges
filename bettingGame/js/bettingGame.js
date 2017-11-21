59236fb5bc658cb70d74f729b0a01789bb90e325
Nnenna Ndukwe <nnenna.s.ndukwe@gmail.com>
2017-11-21 11:53:27 -0500

    the js algo for the slot machine

--- bettingGame/js/bettingGame.js
+++ bettingGame/js/bettingGame.js
@@ -1,24 +1,43 @@
 $(document).ready(function(){
 
 var slot1, slot2, slot3;
-var min = 1;
-var max = 6;
+var storedMoney;
 var arr = ["apples","oranges","cherries","kiwi","pineapples"];
 
 $("#bet1").on("click",function(){
-	for(var i = 0; i < 3; i++){
+
+	var betAmount = $("#betAmount").val();
+	
+	if(betAmount != "" && betAmount > 0){
+	console.log(betAmount)
+	storedMoney = parseFloat(betAmount);
+
+	$("p").text("YOUR BANK: "+storedMoney);
+	$("#betAmount").val("")
 	slot1 = arr[Math.floor(Math.random()*arr.length)];
 	slot2 = arr[Math.floor(Math.random()*arr.length)];
 	slot3 = arr[Math.floor(Math.random()*arr.length)];
-	console.log(slot1);
-}
+	
+	console.log(slot1,slot2,slot3);
 	if((slot1 == slot2) && (slot2 == slot3)){
 		console.log("YOU WIN!");
 	}
-	else{
-		console.log("YOU LOSE!");
+	else if((slot1 == slot2) || (slot2 == slot3) || (slot1 == slot3)){
+		console.log("YOU...KINDA WON? HERE'S 2x YOUR BET.")
+		betAmount = betAmount*2;
+		storedMoney+=betAmount;
+		console.log("Stored Money: "+storedMoney)
 	}
-
+	else{
+		console.log("YOU LOSE! SORRY, BUDDY.");
+		storedMoney-=betAmount;
+		console.log("Stored Money: "+storedMoney)
+	}
+	}
+	else{
+		$("p").text("Please enter in your bet amount!").css("color","red").css("font-weight","bold")
+		$("#betAmount").val("")
+	}
 
 });
 });
